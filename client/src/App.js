4be5540911a4c3014aaaf61f28fc29e34d5a8f2d
zafar-m3n <thezafar.m3n@gmail.com>
2024-04-14 16:22:01 +0530

    Create React app and code cleanup

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="bg-dark">
+        <h1 className="text-success text-center">Dark Mode</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
